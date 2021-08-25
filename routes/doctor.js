f56bbca83bd5c8420229d055d55dcb08bdf57aa2
vpmaharani09 <vpmaharani09@gmail.com>
2021-08-25 19:49:56 +0700

    vpmaharani09

--- routes/doctor.js
+++ routes/doctor.js
@@ -2,11 +2,14 @@ const express = require("express");
 
 const doctorRouter = express.Router();
 
+doctorRouter
+  .get("/", (req, res) => {
+    res.render("web");
+  })
+  .get("/add", (req, res) => {})
+  .post("/add", (req, res) => {})
+  .get("/:id/edit", (req, res) => {})
+  .post("/:id/edit", (req, res) => {})
+  .get("/:id/delete", (req, res) => {});
 
-doctorRouter.get('/', (req, res) => { res.render("web")})
-    .get('/add', (req, res) => { })
-    .post('/add', (req, res) => {})
-    .get('/:id/edit', (req, res) => { })
-    .post('/:id/edit', (req, res) => {})
-    .get('/:id/delete', (req, res) => {})
-module.exports = {doctorRouter};
+module.exports = { doctorRouter };
